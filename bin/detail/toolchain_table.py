11d30e010f55b3a28386cff4b7d0699e2ab73ebe
Alexander Lenhardt <alexander.lenhardt@soundcloud.com>
2017-11-27 17:01:59 +0100

    added soundcloud toolchain for SDK11 and deployment target 9.0

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -357,6 +357,7 @@ if platform.system() == 'Darwin':
       Toolchain('ios-7-1', 'Xcode', ios_version='7.1'),
       Toolchain('ios-7-0', 'Xcode', ios_version='7.0'),
       Toolchain('ios-nocodesign', 'Xcode', nocodesign=True),
+      Toolchain('ios-soundcloud-nocodesign', 'Xcode', nocodesign=True),
       Toolchain('ios-nocodesign-arm64', 'Xcode', ios_version='8.1', nocodesign=True),
       Toolchain('ios-nocodesign-armv7', 'Xcode', ios_version='8.1', nocodesign=True),
       Toolchain('ios-nocodesign-hid-sections', 'Xcode', ios_version='8.1', nocodesign=True),
